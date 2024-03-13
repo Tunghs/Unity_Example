8307041c048d57a4898116ec57052e8b56cac84f
Tunghs <juk08843@gmail.com>
2024-03-13 10:08:14 +0900

    Feat: stage에 따른background의 변화를 주기 위해 backgroundchanger 클래스 추가

---
+++ TrashFlight/Assets/Scripts/BackgroundChanger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChanger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] backgrounds;
+    private int backgroundIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeBackground(float posY, int index, float moveSpeed)
+    {
+
+    }
+}
